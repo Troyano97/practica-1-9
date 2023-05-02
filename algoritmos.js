01b14fc904320534f6f2425bac8d12c747b09047
¨troyano97¨ <ivanferreyradaniel@gmail.com>
2023-05-02 11:31:12 -0300

    primer commit

---
+++ algoritmos.js
@@ -0,0 +1,11 @@
+const cuerpoPagina = document.querySelector("body");
+
+
+
+
+function modificarPag() {
+    cuerpoPagina.style.color= "black";
+    cuerpoPagina.style.background = "white";
+    cuerpoPagina.style.fontFamily = "arial";
+    cuerpoPagina.style.fontSize = "35px";
+}
